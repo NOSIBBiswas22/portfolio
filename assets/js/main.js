1282d48813ed4d8b9d27b48ef4c217b2d025deb6
Nosib Biswas <88088042+NOSIBBiswas22@users.noreply.github.com>
2022-04-08 14:58:51 +0600

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -71,8 +71,9 @@ sr.reveal('.portfolio__img', {interval: 200})
 
 /*SCROLL CONTACT*/
 sr.reveal('.contact__subtitle', {})
-sr.reveal('.contact__icon', {interval: 200})
+sr.reveal('.contact__icon', {interval: 100})
 sr.reveal('.contact__text', {interval: 200})
+sr.reveal('.contact__name', {interval: 200})
 sr.reveal('.contact__input', {delay: 400})
 sr.reveal('.contact__button', {delay: 600})
 
